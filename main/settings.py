75be519314169a7482a59e91de91e59dd5710e6a
arash <arash@gmail.com>
2025-07-07 04:20:20 -0700

    render commit

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^6czny_&v!1(d1iyz58ff6ph7u(xd!!-y2w&@af_)qt(r0!6wi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -119,7 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #### local
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
